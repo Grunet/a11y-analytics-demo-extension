54ec1511b1620d0e6bc706ee245f330f49109a4c
Grunet <sadikshahidain@gmail.com>
2023-02-26 20:55:02 -0600

    add in keyboard detection

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,70 +1,118 @@
-
-console.log(`A11y Analytics Demo - Start time: ${performance.now()}`);
 captureAndEmitData();
-console.log(`A11y Analytics Demo - End time: ${performance.now()}`);
 
 function captureAndEmitData() {
-    console.log("Attributes to segment by");
+  console.log("Attributes to segment by");
 
-    // https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-reduced-motion
-    logMediaFeatureBasedPreference({
-        mediaFeature: "prefers-reduced-motion", 
-        possibleValues: ["no-preference", "reduce"]
-    });
+  captureAndEmitKeyboardData();
 
-    // https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-color-scheme
-    logMediaFeatureBasedPreference({
-        mediaFeature: "prefers-color-scheme", 
-        possibleValues: ["light", "dark"]
-    });
-
-    // https://developer.mozilla.org/en-US/docs/Web/CSS/@media/inverted-colors - Safari only currently
-    logMediaFeatureBasedPreference({
-        mediaFeature: "inverted-colors", 
-        possibleValues: ["none", "inverted"]
-    });
-
-    // https://developer.mozilla.org/en-US/docs/Web/CSS/@media/forced-colors
-    logMediaFeatureBasedPreference({
-        mediaFeature: "forced-colors", 
-        possibleValues: ["none", "active"]
-    });
+  captureAndEmitMediaFeatureData();
 }
 
-function logMediaFeatureBasedPreference({mediaFeature, possibleValues}) {
-    if (checkIfBrowserSupportsMediaFeature({mediaFeature}) === false) {
-        return;
+function captureAndEmitKeyboardData() {
+  const intervalId = setInterval(function checkForKeyboardUsage() {
+    // console.log(
+    //   `A11y Analytics Demo - Keyboard Detection - Start time: ${performance.now()}`
+    // );
+
+    const focusedElement = document.querySelector(":focus-visible");
+
+    if (!focusedElement) {
+      return;
     }
 
-    const resolvedMediaQueries = possibleValues.map((possibleValue) => {
-        return {
-            possibleValue, 
-            mediaQueryResult: window.matchMedia(`(${mediaFeature}: ${possibleValue})`).matches
-        };
-    });
-
-    const mediaQueryThatResolvedToTrue = resolvedMediaQueries.find(({possibleValue, mediaQueryResult}) => mediaQueryResult === true);
-
-    if (mediaQueryThatResolvedToTrue === undefined) {
-        console.log(`Something went wrong. Is there a new ${mediaFeature} allowed value not accounted for here?`);
-        return;
+    // Ignore common false positives for click/touch users
+    const tagNameUpperCased = focusedElement.tagName.toUpperCase();
+    if (tagNameUpperCased === "INPUT" || tagNameUpperCased === "TEXTAREA") {
+      return;
     }
 
-    const preferredValue = mediaQueryThatResolvedToTrue.possibleValue;
+    if (focusedElement.contentEditable === "true") {
+      return;
+    }
 
-    console.log(`${mediaFeature}: ${preferredValue}`);
+    console.log(`uses-keyboard: true`);
+
+    clearInterval(intervalId);
+
+    // console.log(
+    //   `A11y Analytics Demo - Keyboard Detection - End time: ${performance.now()}`
+    // ); // Around 1 ms
+  }, 500);
 }
 
-function checkIfBrowserSupportsMediaFeature({mediaFeature}) {
-    const isMediaFeatureSupportedByTheBrowser = window.matchMedia(
-        `not all and (${mediaFeature}), (${mediaFeature})`
-    ).matches;
+function captureAndEmitMediaFeatureData() {
+  //   console.log(
+  //     `A11y Analytics Demo - Media Features - Start time: ${performance.now()}`
+  //   );
 
-    if (!isMediaFeatureSupportedByTheBrowser) {
-        console.log(`Your browser doesn't support ${mediaFeature} yet`);
+  // https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-reduced-motion
+  logMediaFeatureBasedPreference({
+    mediaFeature: "prefers-reduced-motion",
+    possibleValues: ["no-preference", "reduce"],
+  });
 
-        return false;
-    }
+  // https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-color-scheme
+  logMediaFeatureBasedPreference({
+    mediaFeature: "prefers-color-scheme",
+    possibleValues: ["light", "dark"],
+  });
 
-    return true;
+  // https://developer.mozilla.org/en-US/docs/Web/CSS/@media/inverted-colors - Safari only currently
+  logMediaFeatureBasedPreference({
+    mediaFeature: "inverted-colors",
+    possibleValues: ["none", "inverted"],
+  });
+
+  // https://developer.mozilla.org/en-US/docs/Web/CSS/@media/forced-colors
+  logMediaFeatureBasedPreference({
+    mediaFeature: "forced-colors",
+    possibleValues: ["none", "active"],
+  });
+
+  //   console.log(
+  //     `A11y Analytics Demo - Media Features - End time: ${performance.now()}`
+  //   ); // Around 10ms
+}
+
+function logMediaFeatureBasedPreference({ mediaFeature, possibleValues }) {
+  if (checkIfBrowserSupportsMediaFeature({ mediaFeature }) === false) {
+    return;
+  }
+
+  const resolvedMediaQueries = possibleValues.map((possibleValue) => {
+    return {
+      possibleValue,
+      mediaQueryResult: window.matchMedia(`(${mediaFeature}: ${possibleValue})`)
+        .matches,
+    };
+  });
+
+  const mediaQueryThatResolvedToTrue = resolvedMediaQueries.find(
+    ({ possibleValue, mediaQueryResult }) => mediaQueryResult === true
+  );
+
+  if (mediaQueryThatResolvedToTrue === undefined) {
+    console.log(
+      `Something went wrong. Is there a new ${mediaFeature} allowed value not accounted for here?`
+    );
+    return;
+  }
+
+  const preferredValue = mediaQueryThatResolvedToTrue.possibleValue;
+
+  console.log(`${mediaFeature}: ${preferredValue}`);
+}
+
+function checkIfBrowserSupportsMediaFeature({ mediaFeature }) {
+  const isMediaFeatureSupportedByTheBrowser = window.matchMedia(
+    `not all and (${mediaFeature}), (${mediaFeature})`
+  ).matches;
+
+  if (!isMediaFeatureSupportedByTheBrowser) {
+    console.log(`Your browser doesn't support ${mediaFeature} yet`);
+
+    return false;
+  }
+
+  return true;
 }
